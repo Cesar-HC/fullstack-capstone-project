3607a54b4e0aa3dd2065e66a6b7d2a539a463995
Cesar-HC <u20231e202@upc.edu.pe>
2026-08-23 06:21:16 -0400

    Your message here

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './LoginPage.css';
 
+// Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import { urlConfig } from '../../config';
+
+// Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context/AuthContext';
+
+// Task 3: Import useNavigate from `react-router-dom` to handle navigation
+import { useNavigate } from 'react-router-dom';
+
 function LoginPage() {
-    // Definición de las variables de estado para el formulario
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    // Task 4: Include a state for incorrect password.
+    const [incorrect, setIncorrect] = useState('');
 
-    // Función que se ejecuta al hacer clic en el botón de login
+    // Task 5: Create a local variable for navigate, bearerToken and setIsLoggedIn.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    // Task 6: If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app');
+        }
+    }, [navigate]);
+
     const handleLogin = async () => {
-        console.log("Inside handleLogin");
+        try {
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                // Task 7: Set method
+                method: 'POST',
+                // Task 8: Set headers
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', 
+                },
+                // Task 9: Set body to send user details
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                })
+            });
+
+            // Step 2 - Task 1: Access data coming from fetch API
+            const json = await response.json();
+
+            // Step 2 - Task 5: Check if the token exists (successful login) or set error
+            if (json.authtoken) {
+                // Step 2 - Task 2: Set user details
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+                
+                // Step 2 - Task 3: Set the user's state to log in
+                setIsLoggedIn(true);
+                
+                // Step 2 - Task 4: Navigate to the MainPage after logging in
+                navigate('/app');
+            } else {
+                // Step 2 - Task 5: Clear input and set an error message
+                document.getElementById("email").value = "";
+                document.getElementById("password").value = "";
+                setEmail(''); // Recommended React practice to keep state in sync
+                setPassword(''); // Recommended React practice to keep state in sync
+                
+                setIncorrect("Wrong password. Try again.");
+                
+                // Clear out error message after 2 seconds
+                setTimeout(() => {
+                    setIncorrect("");
+                }, 2000);
+            }
+
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
+            setIncorrect(e.message);
+        }
     };
 
     return (
@@ -17,13 +88,18 @@ function LoginPage() {
                 <div className="col-md-6 col-lg-4">
                     <div className="login-card p-4 border rounded">
                         <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+                        
+                        {/* Step 2 - Task 6: Display an error message to the user */}
+                        <span style={{color:'red', height:'.5cm', display:'block', fontStyle:'italic', fontSize:'12px'}}>
+                            {incorrect}
+                        </span>
 
                         {/* Input para Email */}
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
                             <input
                                 id="email"
-                                type="text"
+                                type="email"
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
@@ -32,7 +108,7 @@ function LoginPage() {
                         </div>
 
                         {/* Input para Password */}
-                        <div className="mb-3">
+                        <div className="mb-4">
                             <label htmlFor="password" className="form-label">Password</label>
                             <input
                                 id="password"
@@ -50,9 +126,8 @@ function LoginPage() {
                         </button>
 
                         <p className="mt-4 text-center">
-                            New here? <a href="/app/register" className="text-primary">Register Here</a>
+                            New here? <a href="/app/register" className="text-primary">Register</a>
                         </p>
-
                     </div>
                 </div>
             </div>
